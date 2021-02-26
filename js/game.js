65be21d8a56368b766d8ded2d18673881cb507a8
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2021-02-25 21:41:48 -0800

    update

--- js/game.js
+++ js/game.js
@@ -1,15 +1,16 @@
-const game = new Phaser.Game(1000, 640, Phaser.CANVAS, 'game', {
+const game = new Phaser.Game(1200,752, Phaser.CANVAS, 'game', {
   preload,
   create,
 });
 
 function create() {
   var x  = 0;
-  for (var clip = 1; clip < 7; clip++) {
+  for (var clip = 1; clip < 6; clip++) {
     var item = game.add.sprite(x, y, 'sheet'+clip);
     item.animations.add('run');
     item.animations.play('run', 15, true);
     if(clip==3) {y+=item.height;x=0;}
+    else
     x+=item.width;
  }
 }
--- js/preload.js
+++ js/preload.js
@@ -1,6 +1,7 @@
 function preload() {
 this.load.path = '../assets/images/';
-for (let index = 1; index < 7; index++) {
-  this.load.spritesheet('sheet'+index,'sheet'+index+'.png',365,360);
+
+for (let index = 1; index <6; index++) {
+  this.load.spritesheet('sheet'+index,'sheet'+index+'.png',400,376);
 }
 }
